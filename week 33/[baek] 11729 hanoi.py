6775d799a7a6d5430730c26945a289624bd27b95
lim <tbnsok40@naver.com>
2020-07-05 01:12:37 +0900

    'green_hanoi'

--- week 33/[baek] 11729 hanoi.py
+++ week 33/[baek] 11729 hanoi.py
@@ -11,12 +11,18 @@
 #     hanoi(n-1, aux, to, fromm)
 
 #2 begin, aux, end : 1, 2, 3
+n = int(input())
+block = list()
 def hanoi(n, begin, aux, end):
     if n == 1:
-        print(begin, end)
+        block.append([begin, end])
     else:
         hanoi(n-1, begin, end, aux)
         hanoi(1, begin, aux, end)
         hanoi(n-1, aux, begin, end)
 
-hanoi(3,1,2,3)
+hanoi(n,1,2,3)
+
+print(len(block))
+for i in block:
+    print(*i)
