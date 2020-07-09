260d65faff2af09b4ea845b51d135af893a4e17d
lim <tbnsok40@naver.com>
2020-07-09 17:11:34 +0900

    '풀엇다시불'

--- week 34/[baek] 2993.py
+++ week 34/[baek] 2993.py
@@ -1,15 +1,21 @@
 # strings = list(input())
 # print(strings) # list는 모두 한letter단위로 반환한다.
-import sys
-def read(): return sys.stdin.readline()
+# import sys
+# def read(): return sys.stdin.readline().rstrip()
+#readline을 쓰면 개행을 같이 읽어들인다.
+before = input()
+final = before
+for i in range(len(before)-2):
 
-before = read()
-# for i in range(len(before)):
-#     for j in range(i+2,len(before)):
-#         first = before[:i+1]
-#         mid = before[i+1:j]
-#         last = before[j:]
-#         first.reverse()
+    for j in range(i+2, len(before)):
+        fir = before[:i+1][::-1]
+        sec = before[i+1:j][::-1]
+        thr = before[j:][::-1]
+        full = fir + sec + thr
+        # print('full: ',full, 'before:',before)
 
-print(('').join(reversed(before)))
-print(before[::-1])
+        # if full < before:
+        #     final = full
+        if full < final:
+            final = full
+print(final)
