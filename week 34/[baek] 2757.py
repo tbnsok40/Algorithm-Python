e644ee103a5871d591c03d50f06e940ecf3aa8c0
lim <tbnsok40@naver.com>
2020-07-10 17:30:11 +0900

    '개어렵다'

---
+++ week 34/[baek] 2757.py
@@ -0,0 +1,12 @@
+import sys
+def read(): return sys.stdin.readline()
+inputs = read().strip('R')
+print(inputs)
+print(inputs.rstrip('C'))
+
+# print(chr(col + 64))
+# # col_answer = (col//27) + (col % 27)
+# # num = int2str(col)
+# # print(num)
+# print(chr(65))
+
