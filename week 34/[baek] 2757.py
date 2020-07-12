a0cc01bd4eb4a7e1c118678b89a06be044805c6b
lim <tbnsok40@naver.com>
2020-07-12 16:20:40 +0900

    'strip,split'

--- week 34/[baek] 2757.py
+++ week 34/[baek] 2757.py
@@ -1,9 +1,13 @@
-import sys
-def read(): return sys.stdin.readline()
-inputs = read().strip('R')
-print(inputs)
-print(inputs.rstrip('C'))
 
+inputs = input()
+print('inputs:',(inputs))
+inputs =inputs.strip('R') # 중간letter는 왜 strip안돼?
+print(inputs)
+inputs = inputs.replace('C',',')
+inputs = list(inputs.split(','))
+print(inputs)
+
+# print(inputs.strip('C'))
 # print(chr(col + 64))
 # # col_answer = (col//27) + (col % 27)
 # # num = int2str(col)
--- week 34/[baek] 3187.py
+++ week 34/[baek] 3187.py
@@ -1,43 +1,44 @@
-from collections import deque
 import sys
+from collections import deque
 
-input = sys.stdin.readline()
+input = sys.stdin.readline
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
+def bfs(x,y):
+    v_cnt = 0
+    k_cnt = 0
+    visited[x][y] =1 # 까먹지마라
+    q.append([x,y])
+    while q:
+        x,y = q.popleft()
+        if a[x][y] == 'v':
+            v_cnt += 1
+        elif a[x][y] =='k':
+            k_cnt += 1
+        for i in range(4):
+            new_x = x + dx[i]
+            new_y = y + dy[i]
+
+            if (0<= new_x < n) and (0<= new_y<m): # 여기 new_x의 범위가 왜 m(세로길이)와 비교돼야 하는지, new_y도 마찬가지
+                if a[new_x][new_y] != '#' and (visited[new_x][new_y]==0):
+                    visited[new_x][new_y] =1
+                    q.append([new_x,new_y])
+    if v_cnt>=k_cnt:
+        k_cnt=0
+    else:
+        v_cnt =0
+    return [v_cnt,k_cnt]
+
 m, n = map(int, input().split())
-a = [list(input().split() for _ in range(m))]
+a = [list(input().strip()) for _ in range(m)]
 visited = [[0]*n for _ in range(m)]
 q = deque()
 
 v, k = 0,0
 for i in range(m):
     for j in range(n):
-        if (a[i][j] != '#') and (visited[i][j] == 0):
+        if a[i][j] != '#' and (visited[i][j]==0):
             v_cnt, k_cnt = bfs(i,j)
-            v += v_cnt
-            k += k_cnt
+            v += v_cnt; k+=k_cnt
 print(k,v)
-
-def bfs(x,y):
-    q.append([x,y])
-    v_cnt, k_cnt = 0,0
-    while q:
-        x,y = q.popleft()
-        if a[x][y] == 'v':
-            v_cnt += 1
-        elif a[x][y] == 'k':
-            k_cnt += 1
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<= nx < m and 0 <= ny < n:
-                visited[nx][ny] = 1 # visited
-                q.append([nx,ny]) # 이런 구조의 bfs는, 이미 지났던 점에 대해서 또 추가 되는 경우는 있을 수 없나?
-    if v_cnt >= k_cnt:
-        k_cnt =0
-    else:
-        v_cnt = 0
-    return [v_cnt,k_cnt]
-
-
