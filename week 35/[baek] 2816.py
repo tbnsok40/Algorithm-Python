e44b17de130248861128f914ff0489447db34fef
lim <tbnsok40@naver.com>
2020-07-16 15:40:54 +0900

    'not_yet'

--- week 35/[baek] 2816.py
+++ week 35/[baek] 2816.py
@@ -1,11 +1,3 @@
 T = int(input())
 li = list()
-for _ in range(T):
-    channel = input()
-    li.append(channel)
-
-for i in range(li[i]):
-    if li[i+1] == 'KBS1':
-        print(3)
-        for j in range(i+1)
-
+# not yet
