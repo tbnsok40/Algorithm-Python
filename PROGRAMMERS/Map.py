7a4f920c0616f97acf5aaf56df2e7307b1dae7cf
lim <tbnsok40@naver.com>
2020-07-22 01:37:41 +0900

    'join'

--- PROGRAMMERS/Map.py
+++ PROGRAMMERS/Map.py
@@ -1,3 +1,10 @@
-list2 = ['1', '100', '33']
-# print(*list2)
-print(list(map(int, list2)))
+1
+list1 = ['1', '100', '33']
+print(list(map(int, list1)))
+
+2
+list2 = [[1,2],[3,4],[5]]
+def solution(mylist):
+    answer = list(map(list.__len__, mylist))
+    return answer
+print(solution(list2))
