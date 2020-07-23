81efd2375046749252f6e512250d1043acf340c9
lim <tbnsok40@naver.com>
2020-07-23 20:10:16 +0900

    ''

---
+++ PROGRAMMERS/Try except.py
@@ -0,0 +1,16 @@
+strings = input()
+strings = list(strings) #리스트화
+diction = dict()
+
+for i in strings:
+    try:
+        diction[i] += 1
+    except KeyError as e:
+        diction[i] = 1
+        print(e)
+print(diction)
+
+
+
+
+
