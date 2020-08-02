44914dfa83c443a5c091ee14ced8e9e992468c44
lim <tbnsok40@naver.com>
2020-08-03 01:21:30 +0900

    '0000처리'

--- PROGRAMMERS/Sort/가장 큰 수.py
+++ PROGRAMMERS/Sort/가장 큰 수.py
@@ -20,12 +20,27 @@ li = [3,30,34,9,100,200,1000]
 #     return ('').join(one[::-1]) + ('').join(dec[::-1])+ ('').join(hund[::-1])+('').join(thou[::-1])
 
 li = [3, 330, 34, 5, 9,1000]
+# # 지린다
+# def solution(numbers):
+#     numbers = list(map(str, numbers))
+#     numbers.sort(key=lambda x: x*3, reverse=True)
+#     return str(int(''.join(numbers)))
+# print(solution(li))
 
-def solution(numbers):
-    numbers = list(map(str, numbers))
-    print(numbers)
-    numbers.sort(key=lambda x: x*3, reverse=True)
-    print(numbers)
-    return str(int(''.join(numbers)))
-# 지린다
+
+
+
+
+
+
+
+li = [3, 330, 34, 5, 9,1000]
+def solution(li):
+
+    li =list(map(str, li))
+    # li.sort(key=lambda x: x*3, reverse=True)
+    li = sorted(li, key=lambda x: x*3, reverse=True)
+    return str(int(('').join(li)))
+
+
 print(solution(li))
