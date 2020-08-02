a59ef9758e782309c3d05ebfe71b679436bf95b5
lim <tbnsok40@naver.com>
2020-08-02 16:29:00 +0900

    'sort-

--- PROGRAMMERS/Sort/가장 큰 수.py
+++ PROGRAMMERS/Sort/가장 큰 수.py
@@ -1,13 +1,31 @@
-li = [3,30,34,9]
-li2 = []
-one = []
-dec = []
-hund = []
-thou = []
-# for i in li:
-#     if i < 10:
-li.sort()
-for idx, i in enumerate(li):
-    if i > 10:
-        li[:idx]
+# 에러 존재
+li = [3,30,34,9,100,200,1000]
+# 정렬
+# 십의 단위로 indexing
+# def solution(li):
+#     one = []
+#     dec = []
+#     hund = []
+#     thou = []
+#     li.sort()
+#     for idx, i in enumerate(li):
+#         if (i) < 10:
+#             one.append(str(i))
+#         elif (10<=i)and(i<100):
+#             dec.append(str(i))
+#         elif (100<=i) and (i<1000):
+#             hund.append(str(i))
+#         else:
+#             thou.append(str(i))
+#     return ('').join(one[::-1]) + ('').join(dec[::-1])+ ('').join(hund[::-1])+('').join(thou[::-1])
 
+li = [3, 330, 34, 5, 9,1000]
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    print(numbers)
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    print(numbers)
+    return str(int(''.join(numbers)))
+# 지린다
+print(solution(li))
