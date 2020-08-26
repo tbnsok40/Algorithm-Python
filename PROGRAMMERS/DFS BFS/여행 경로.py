fc05ce150ca3c3b75cb967428ed02b03fb7b46fc
lim <tbnsok40@naver.com>
2020-08-27 00:47:57 +0900

    'ppt그림참조'

--- PROGRAMMERS/DFS BFS/여행 경로.py
+++ PROGRAMMERS/DFS BFS/여행 경로.py
@@ -1,5 +1,5 @@
-# tickets = [['ICN', 'SFO'], ['ICN', 'ATL'], ['SFO', 'ATL'], ['ATL', 'ICN'], ['ATL','SFO']]
-tickets = [['ICN', 'JFK'], ['HND', 'IAD'], ['JFK', 'HND']]
+tickets = [['ICN', 'SFO'], ['ICN', 'ATL'], ['SFO', 'ATL'], ['ATL', 'ICN'], ['ATL','SFO']]
+# tickets = [['ICN', 'JFK'], ['HND', 'IAD'], ['JFK', 'HND']]
 from collections import deque as queue
 def solution(tickets):
     # dictionary 정렬/정리
@@ -10,17 +10,19 @@ def solution(tickets):
         except:
             d[from_] = [to_]
     for key_ in d.keys():
-        d[key_].sort()
-    ###################
-    answer.append('ICN')
-    curr = answer[-1]
-
-    while len(answer) < len(tickets)+1:
-        curr = d[curr].pop(0)
-        answer.append(curr)
-        curr = answer[-1]
-        # print(d)
-    return answer
+        d[key_].sort(reverse=True)
+    print('dict:',d)
+    ######################################
+    stack = ['ICN']
+    path = []
+    while len(stack) > 0:
+        top = stack[-1]
+        if top not in d or len(d[top]) == 0:
+            path.append(stack.pop())
+        else:
+            # print('answer',stack)
+            stack.append(d[top].pop())
+    return path[::-1]
 print(solution(tickets))
 
-# genres_in_order = sorted(songs_in_genres.keys(), key=lambda x: songs_in_genres[x], reverse=True)
+
