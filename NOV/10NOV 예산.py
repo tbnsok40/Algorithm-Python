ec70e1de22f3ddf9e09dac52625f4392f28352b0
lim <tbnsok40@naver.com>
2020-11-11 10:08:56 +0900

    '10NOV미해결'

--- NOV/10NOV 예산.py
+++ NOV/10NOV 예산.py
@@ -3,10 +3,23 @@ def solution(d, budget):
     if sum(d) == budget:
         return len(d)
 
-d = [1,3,2,5,4]
-budget = 9
+# d = [1,3,2,5,4]
+# budget = 9
+#
+# d = [2,2,3,3]
+# budget = 10
+# print(solution(d,budget))
 
-d = [2,2,3,3]
-budget = 10
-print(solution(d,budget))
 
+# import math
+# def solution(n):
+#     if math.sqrt(n) % 1 != 0:
+#         return -1
+#     else:
+#         return int((math.sqrt(n) + 1)**2)
+#
+#     if int(math.sqrt(n)) == math.sqrt(n):
+#         return (int(math.sqrt(n))+1)**2
+#     else:
+#         return -1
+# print(solution(121))
