fdc78b6b4c9d8e7bcab06795d2f7f6af2605051d
임성후 <lim@imseonghuui-MacBookPro.local>
2020-11-21 13:34:08 +0900

    19NOV testcase 5,8 missing

--- NOV/19NOV.py
+++ NOV/19NOV.py
@@ -2,17 +2,16 @@
 # 두자리수는 어차피 10 하나
 # 1이 나오면 1인지 10인지 확인 => 1 다음이 int형이면 10인거지
 def solution(dartResult):
-    char_set = ('s', 'd', 't')
-    sign_set = ('*','#')
     temp = []
-    temp2 = []
     for letter in dartResult:
 
         try:
-            if letter == '0':
+            if letter == '0' and temp[-1] == 1:
                 temp.pop()
                 temp.append(10) # 이전의 1은 제거해줘야한다.
                 continue
+            elif letter == '0' and temp[-1] != 1:
+                pass
             letter = int(letter)
             temp.append(letter)
 
@@ -20,13 +19,30 @@ def solution(dartResult):
             num = temp.pop()
             if letter == "D":
                 num = num ** 2
-            elif letter == 'S':
+                temp.append(num)
+            elif letter == 'T':
                 num = num ** 3
-            temp.append(num)
+                temp.append(num)
+            elif letter == 'S':
+                temp.append(num)
 
+            try:
+                if letter == '*' and temp:
+                    num = num * 2
+                    num2 = temp.pop() * 2
+                    temp.append(num2)
+                    temp.append(num)
+                elif letter == '*' and not temp:
+                    num = num * 2
+                    temp.append(num)
+                elif letter == '#':
+                    num = num * (-1)
+                    temp.append(num)
+            except IndexError:
+                pass
+    return sum(temp)
 
-    return temp
-
-dartResult = '1D#10S*3S'
+dartResult = '1D#2S*3S'
+# dartResult = '1S*2T*3S'
+dartResult = '1D2S0T'
 print(solution(dartResult))
-
