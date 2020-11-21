7dce53629259260fdd0e6c867fb6c4ddce319ba6
임성후 <lim@imseonghuui-MacBookPro.local>
2020-11-21 11:41:38 +0900

    18NOV retry

--- NOV/18NOV 시저암호.py
+++ NOV/18NOV 시저암호.py
@@ -1,29 +1,8 @@
 def solution(s, n):
-    answer = ''
-    return answer
-
-n = 4
-s = "a B z"
-# print(solution(s,n))
-
-s='a z'
-d = 'A Z'
-for i in s:
-    print(ord(i))
-
-for i in d:
-    print(ord(i))
-
-# 공백은 32
-# a~z: 97 ~ 122
-# A~Z: 65 ~ 90
-
-# 90 + 1 = 91 = 65
-# 90 + 2 = 92 = 66
-
-# 91 - 26 = 65
-n % 26 만큼 움직여야
-if i + (n % 26) <= 90:
-    i + (n % 26)
-else:
-    (i + (n % 26)) % 90 + 65
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i]=chr((ord(s[i])-ord('A')+ n)%26+ord('A'))
+        elif s[i].islower():
+            s[i]=chr((ord(s[i])-ord('a')+ n)%26+ord('a'))
+    return "".join(s)
