880c96fdcfd928a70d68a3863aa27c814fc27761
tbnsok40 <tbnsok40@naver.com>
2020-11-25 22:46:56 +0900

    cant_understand

--- NOV/23NOV 124나라의숫자.py
+++ NOV/23NOV 124나라의숫자.py
@@ -1,14 +1,5 @@
 def solution(n):
-
-    a, b = divmod(n, 3)
-    if b == 0:
-        last = "4"
-    elif b == 2:
-        last = "2"
-    else:
-        last = "1"
-
-
-
-    answer = ''
-    return answer
+    if n < 3:
+        return '124'[n]
+    a, b = divmod(n - 1, 3)
+    return solution(a) + '124'[b]
