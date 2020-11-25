0768c0b71fcaa79359fcf087939d4a28e16bef4e
tbnsok40 <tbnsok40@naver.com>
2020-11-26 00:23:38 +0900

    25NOV update

--- NOV/25NOV 스킬트리.py
+++ NOV/25NOV 스킬트리.py
@@ -1,20 +1,17 @@
 def solution(skill, skill_trees):
     hash_dict = {}
+    count = 0
+
     for idx, i in enumerate(skill):
         hash_dict[i] = idx
-    count = 0
-    for skills in skill_trees: # O(n)
-        temp = [-1]
 
+    for skills in skill_trees:
+        temp = [-1]
         for j in skills:
             if (j in hash_dict) and (hash_dict[j] > max(temp)):
                 temp.append(hash_dict[j])
-        # print(skills, temp)
+
         if len(temp) - 2 == max(temp):
-            # print(skills,temp)
             count += 1
 
     return count
-skill = "CBD"
-skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
-print(solution(skill, skill_trees))
