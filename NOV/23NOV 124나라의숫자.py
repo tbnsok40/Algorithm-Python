8217c497f0f20b11526ad04b2c5ebf092f265b79
0828namjin <0828namjin@naver.com>
2020-11-27 16:24:42 +0900

    27NOV 기능개발

--- NOV/23NOV 124나라의숫자.py
+++ NOV/23NOV 124나라의숫자.py
@@ -3,3 +3,6 @@ def solution(n):
         return '124'[n]
     a, b = divmod(n - 1, 3)
     return solution(a) + '124'[b]
+
+
+print(solution(12))
---
+++ NOV/27NOV 기능개발.py
@@ -0,0 +1,25 @@
+import math
+from collections import deque
+
+# deque than slicing
+def solution(progresses, speeds):
+
+    temp = [100 - i for i in progresses]
+    sub = deque([math.ceil(a/b) for a, b in zip(temp, speeds)])
+    result = []
+
+    while sub:
+        count = 1
+        head = sub.popleft()
+        while sub:
+            try:
+                if head >= sub[0]:
+                    count += 1
+                    sub.popleft()
+                else:
+                    result.append(count)
+                    break
+            except:
+                pass
+    result.append(count)
+    return result
