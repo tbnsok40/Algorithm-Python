6bc2a528fd6b047753c6087e6bf79879d8b5c286
tbnsok40 <tbnsok40@naver.com>
2020-12-09 16:43:41 +0900

    06DEC

--- DEC/06DEC 구명보트.py
+++ DEC/06DEC 구명보트.py
@@ -8,41 +8,48 @@ limit = 100
 
 people = [15, 20, 30, 40, 45, 50, 60,85,90]
 limit = 100
-from collections import deque
-from itertools import islice
 
 def solution(people, limit):
-    #1 오름차순 정렬
-    #2 리턴값을 최대한 작게
-    #3 작은 배열에 담는다 리미트 안 때릴때가지
-
-    # 발상 전환: 오름차순 대신 내림차순으로 가보자.
     count = 0
-
     sorted_people = sorted(people)
-    sorted_people = deque(sorted_people)
 
     while sorted_people:
         count += 1
-        temp_people = islice(sorted_people,0, len(sorted_people)-1)
-        # temp_people = sorted_people[:-1]
-
+        last = sorted_people.pop()
+        temp_people = sorted_people
         store = 0
         for i in temp_people:
-            if i + sorted_people[-1] <= limit:
+            if i + last <= limit:
                 store = i
             else:
-                # count += 1
                 break
-
         if store != 0:
             sorted_people.remove(store)
-        # sorted_people.remove(sorted_people[-1])
-        sorted_people.pop()
+        print(sorted_people)
+
+    return count
+
+
+def solution(people, limit):
+    count = 0
+    sorted_people = sorted(people)
+
+    start = 0
+    end = len(sorted_people) - 1
+
+
+    while start <= end:
+        count += 1
+        if sorted_people[start] + sorted_people[end] <= limit:
+            start += 1
+            end -= 1 # 애초에 두명'만' 태울 수 있음에 의하면 end를 빼주는게 맞지.
+        else:
+            end -= 1
 
     return count
 
 # 강제로 deque형 변환 했음에도 효율성을 뚫지 못하기에
 # 풀이 구조 자체를 바꿔야 한다.
-
+# islice()를 배웠네
 print(solution(people, limit))
+# 깊은 고민은 깊은 실력을 만든다.
