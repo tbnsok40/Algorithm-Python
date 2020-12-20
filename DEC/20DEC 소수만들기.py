5e63b9b72cdcb67e12dec7d82c2c3ed9ea965485
tbnsok40 <tbnsok40@naver.com>
2020-12-20 22:44:27 +0900

    20DEC

--- DEC/20DEC 소수만들기.py
+++ DEC/20DEC 소수만들기.py
@@ -1,12 +1,6 @@
 from itertools import combinations
 def solution(nums):
-    N = len(nums)
-    count = 0
-    for i in combinations(nums, 3):
-        if sosu(sum(i)):
-            count += 1
-
-    return count
+    return sum([1 for i in combinations(nums, 3) if sosu(sum(i))])
 
 
 def sosu(nums):
@@ -14,7 +8,3 @@ def sosu(nums):
         if nums % i == 0:
             return False
     return True
-
-
-nums = [1, 2, 7, 6, 4]
-print(solution(nums))
