aeeca2c477e3de420892bf56bcff54bd935c6481
tbnsok40 <tbnsok40@naver.com>
2020-12-21 19:06:14 +0900

    21DEC

--- DEC/21DEC 짝지어 제거하기.py
+++ DEC/21DEC 짝지어 제거하기.py
@@ -1,34 +1,36 @@
-s = 'baabaa'
-# s = 'cdbbdc'
-# s = 'acddcdda'
-# def solution(s):
-#     N = len(s)
-#     front = s[:N//2]
-#     rear = s[N//2:]
-#     if N % 2 != 0:
-#         return 0
-#     if (front == rear) and (N // 2) % 2 != 0:
-#         return 1
-#     elif front == rear[::-1]:
-#         return 1
-#     else:
-#         return 0
+from collections import deque
 
+# s가 list면 시간초과, deque()이면 통
 def solution(s):
-    s = list(s)
-    count = 0
-    N = len(s)
+    temp = []
+    s = deque(list(s))
+    temp.append(s.popleft())
     while s:
-        for idx, i in enumerate(s):
-            try:
-                if s[idx] == s[idx + 1]:
-                    s.remove(s[idx]), s.remove(s[idx])
-                    break
-            except:
-                pass
-        count += 1
-        if count > (N) + 1:
-            return 0
-    return 1
+        try:
+            if temp[-1] == s[0]:
+                temp.pop()
+                s.popleft()
+            else:
+                temp.append(s.popleft())
+        except:
+            temp.append(s.popleft())
 
+    if len(temp) == 0: return 1
+    else: return 0
 print(solution(s))
+
+# 굳이 try~ except 쓰지 않아도 된다.
+def solution(s):
+    answer = list()
+    for i in s:
+        if not(answer):
+            answer.append(i)
+        else:
+            if answer[-1] == i:
+                answer.pop()
+            else:
+                answer.append(i)
+    if answer:
+        return 0
+    else: return 1
+
