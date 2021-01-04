bb1d60a575c1118cdccdaaaf2739d2f2c212bc6e
tbnsok40 <tbnsok40@naver.com>
2021-01-04 18:51:53 +0900

    29DEC

--- DEC/29DEC 점프와순간이동.py
+++ DEC/29DEC 점프와순간이동.py
@@ -1,7 +1,13 @@
 def solution(n):
-    ans = 0
+    cnt = 0
 
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
+    while n > 0:
+        q, r = divmod(n, 2)
+        if r != 0:
+            cnt += 1
+        n = q
+    return cnt
 
-    return ans
+#1. n을 분해해야하나? ㅇㅇ
+#2. n이 0이 되기 전까지 분해
+#3. n % 2
