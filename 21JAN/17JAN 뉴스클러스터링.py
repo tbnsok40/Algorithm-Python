e68fd630d3a4ef6bac4dc873229b5f37ad1a9442
tbnsok40 <tbnsok40@naver.com>
2021-01-17 22:17:11 +0900

    still

--- 21JAN/17JAN 뉴스클러스터링.py
+++ 21JAN/17JAN 뉴스클러스터링.py
@@ -4,6 +4,15 @@ str2 = 'french'
 str1 = "E=M*C^2"
 str2 = "e=m*c^2"
 
+str1 = "AA_bb_aa_AA"
+str2 = "BBB"
+
+str1 = "ABDDD"
+str2 = "DDEFGHH"
+
+str1 = "AACCC"
+str2 = "A A A A A C C C C"
+import math
 def solution(str1, str2):
     str1 = str1.upper()
     str2 = str2.upper()
@@ -13,9 +22,6 @@ def solution(str1, str2):
     temp1 = [(str1[i] + str1[i + 1]) for i in range(len(str1) - 1) if (str1[i] + str1[i + 1]).isalpha()]
     temp2 = [(str2[i] + str2[i + 1]) for i in range(len(str2) - 1) if (str2[i] + str2[i + 1]).isalpha()]
 
-    # 두 집합 사이에서 같은지 비교
-    print(sorted(temp1))
-    print(sorted(temp2))
     uni_temp = []
     for i in temp1:
         for j in temp2:
@@ -24,11 +30,11 @@ def solution(str1, str2):
                 temp2.remove(j)
                 break
     all_temp = temp1 + temp2
-    print(uni_temp, all_temp)
+
     if len(uni_temp) != 0 and len(all_temp) != 0:
-        return int((len(uni_temp) / len(all_temp)) * 65536)
+        return math.floor((len(uni_temp) / len(all_temp)) * 65536)
     else:
-        return 1 * 65536
+        return 65536
 # test 5, 13 error
 print(solution(str1, str2))
 
