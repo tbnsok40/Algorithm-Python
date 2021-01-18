84a4ad693c9e502c33690eabcc0c868dd43241a8
tbnsok40 <tbnsok40@naver.com>
2021-01-19 08:52:38 +0900

    .

--- 21JAN/19후보키.py
+++ 21JAN/19후보키.py
@@ -3,23 +3,29 @@ def solution(relation):
 
     trans_rel = list(map(list, zip(*relation)))
     count = 0
+    print(trans_rel)
+
     for a_list in trans_rel:
         if len(a_list) == len(set(a_list)):
             trans_rel.remove(a_list)
             count += 1
     # 유일성
-    # print(trans_rel)
+    print(trans_rel)
     N = len(trans_rel)
+    doubled_list = []
     for i in range(2, N):
         for j in combinations(trans_rel,i):
             temp_list = []
             for k in zip(*j):
                 temp_list.append(k)
             if len(temp_list) == len(set(temp_list)):
-                # print(temp_list)
+                doubled_list.append(temp_list)
                 count += 1
 
-    # 이제 최소성 만족시키러 ㄲ
+    print(doubled_list)
+# enumerate로 인덱스 뽑아내서 그 인덱스가 중복되면 Count를 안쳐야겠다.
+# 근데 그게 쉽냐고 ;
+# 이제 최소성 만족시키러 ㄲ
 
     # print(count)
     return count
