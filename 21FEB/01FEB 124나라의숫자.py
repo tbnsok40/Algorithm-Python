b8072c25b66f546dd891daafd55c39f5a2961cfb
lim <tbnsok40@naver.com>
2021-02-01 05:52:39 +0900

    '01FEB'

---
+++ 21FEB/01FEB 124나라의숫자.py
@@ -0,0 +1,19 @@
+# 패턴을 보면 3을 단위로  1의 단위에선 같은 숫자가 반복된다. => 재귀가 하나의 방법이 될 수 있다,
+def solution(n):
+    if n <= 3:
+        return '124'[n-1]
+    else:
+        a, b = divmod(n - 1, 3)
+        return solution(a) + '124'[b]
+
+# 반복으로도 풀 수 있다
+def solution(n):
+    num = '124'
+    answer = ''
+    while n > 0:
+        p, r = divmod(n - 1, 3)
+        answer = num[r] + answer
+        n = p
+    return answer
+
+print(solution(15))
