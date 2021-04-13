c10112a213b5173c90f195a20ff794231ad52282
tbnsok40 <tbnsok40@naver.com>
2021-04-13 23:02:40 +0900

    python tricks

---
+++ 21APR/13APR 오픈채팅방.py
@@ -0,0 +1,17 @@
+# def solution(record):
+#     answer = []
+#
+#     return answer
+#
+#
+# record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+#
+# print(solution(record))
+
+
+
+
+# python tricks
+# how to pick only and all even numbers
+a = [1,2,3,4,5,6,7,8,9]
+print(a[1::2])
