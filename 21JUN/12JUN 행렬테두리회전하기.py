4a15b82cd13bca53174e40b3492e9474329396e9
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-06-12 20:19:19 +0900

    안풀리네

---
+++ 21JUN/12JUN 행렬테두리회전하기.py
@@ -0,0 +1,69 @@
+def make_matrix(rows, columns):
+    start = 1
+    mid = 1 + columns
+    matrix = []
+
+    for r in range(1, rows + 1):
+        row = [c for c in range(start, mid)]
+        matrix.append(row)
+        start = mid
+        mid = columns * (r + 1) + 1
+
+    return matrix
+
+
+def solution(rows, columns, queries):
+
+    matrix = make_matrix(rows, columns)
+    # index 일 뿐 값이 아니다. (top 보다 bottom이 크고, left보다 right가 크다)
+    answer = []
+
+    for x1, y1, x2, y2 in queries:
+        top = x1 - 1
+        left = y1 - 1
+        bottom = x2 - 1
+        right = y2 - 1
+
+        temp = matrix[top][left]
+        mini = temp
+
+        for i in range(bottom, top, -1):  # top < bottom # desc
+            value = matrix[i - 1][right]
+            matrix[i][right] = value
+            mini = min(mini, value)
+
+        for i in range(left, right):
+            value = matrix[bottom][i + 1]
+            matrix[bottom][i] = value
+            mini = min(mini, value)
+
+        for i in range(top, bottom):  # top < bottom
+            value = matrix[i + 1][left]
+            matrix[i][left] = value
+            mini = min(mini, value)
+
+        # 얘가 무조건 마지막에 있어야한다. but why?
+        for i in range(right, left, -1):
+            value = matrix[top][i - 1]
+            matrix[top][i] = value
+            mini = min(mini, value)
+
+        matrix[top][left + 1] = temp
+        answer.append(mini)
+
+        for i in matrix:
+            print(i)
+
+    return answer
+
+
+print(solution(6, 6, [[2, 2, 5, 4], [3, 3, 6, 6], [5, 1, 6, 3]]))
+
+# rows:
+# columns:
+# queries:
+
+# 1. rows, columns, 로 매트릭스 만든다.
+# 2. queries 를 한칸씩 돌릴 수 있는 로직 만들어야한다.
+# 3.
+
