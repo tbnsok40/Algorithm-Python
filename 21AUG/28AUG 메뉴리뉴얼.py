e57ec50b677a14eb8bcc99ffc82bb55e0e5b149b
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-08-28 12:58:54 +0900

    메뉴리뉴얼 #2

--- 21AUG/28AUG 메뉴리뉴얼.py
+++ 21AUG/28AUG 메뉴리뉴얼.py
@@ -1,26 +1,25 @@
 from itertools import combinations
+from collections import Counter
 
 
 def solution(orders, course):
     answer = []
     # menu = dict() # initialize
     for n in course:  # 얘가 먼저 나와야 하는 이유는, 갯수별 최강자를 먼저 골라내야하기 때문
-        menu = dict()
+        temp = []
         for order in orders:
-            for combi in combinations(order, n):
-                combi = sorted(combi)
-                menu_candidate = ''.join(combi)
-                try:
-                    menu[menu_candidate] += 1
-                except KeyError:
-                    menu[menu_candidate] = 1
-        max = 2
-        for m, n in menu.items():
-            if n >= max:
-                max = n
-        for m, n in menu.items():
-            if n == max:
-                answer.append(m)
+            for combination in combinations(order, n):
+                temp.append(''.join(sorted(combination)))
+
+        menu = Counter(temp)
+        try:
+            maximum = max(Counter(temp).values())
+        except ValueError:
+            pass
+
+        for key, value in menu.items():
+            if value == maximum and maximum > 1:
+                answer.append(key)
     answer.sort()
     return answer
 
