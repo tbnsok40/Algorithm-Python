bff92f8f75b8742ed456656ec7c514ca87b48060
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-08-29 14:24:00 +0900

    closer to answer

--- 21AUG/29AUG 프렌즈4블록.py
+++ 21AUG/29AUG 프렌즈4블록.py
@@ -3,16 +3,15 @@ import copy
 
 def removeSame(m, n, matrix):
     remove = []
-    # before_matrix = copy.deepcopy(matrix)
 
     for r in range(m):
         for c in range(n):
             c_ = c + 1
             r_ = r + 1
             if c_ < n and r_ < m:
-                if matrix[r][c] != 0 and matrix[r][c] == matrix[r_][c] == matrix[r][c_] == matrix[r_][c_]:
-                    # 복사본 같은걸 만들어서, 조건 만족하는 모든 부분 0으로 만든다음에, 전체 순회.
-                   remove.append((r, c))
+                if matrix[r][c] == matrix[r_][c] == matrix[r][c_] == matrix[r_][c_] and (matrix[r][c] != "-"):
+                    remove.append((r, c))
+    print("len(remove)", len(remove))
     if len(remove) > 0:
         for rem in remove:
             r_, c_ = rem[0], rem[1]
@@ -24,19 +23,51 @@ def removeSame(m, n, matrix):
 
 def solution(m, n, board):
     matrix = []
+    count = 0
     for row in board:
         matrix.append(list(row))
-    while True:
-        a = removeSame(m, n, matrix)
-        if not a:
-            break
-        # break
-        # 한번 삭제되면 이동/ 조정 함수가 필요
+
+    matrix = list(map(list, zip(*matrix)))
+    i = 0
+    while i < 1:
+        if removeSame(n, m, matrix):
+            for t in matrix:
+                while 0 in t:
+                    count += 1
+                    t.remove(0)
+                    t.append("-")
+        else:
+            pass
+        i += 1
+
+    if removeSame(n, m, matrix):
+        for t in matrix:
+            while 0 in t:
+                count += 1
+                t.remove(0)
+                t.append("-")
+
     for m in matrix:
         print(m)
-    answer = 0
-    return answer
 
+    print(count)
+
+    return
+
+
+
+# while True:
+#     a = removeSame(m, n, matrix)
+#     print(matrix)
+#     changed = changeRowColumn(matrix, m) # 한번만 전치시키고 계속 밀어내고 지우면 된다. 그리고 삭제된 것만 카운팅하면되구나. 개쩐
+#     print(2, changed)
+#     if not a:
+#         break
+# break
+# 한번 삭제되면 이동/ 조정 함수가 필요
+# for m in matrix:
+#     print(m)
+# answer = 0
 
 # 2 x 2 면 지워지는 함수
 
