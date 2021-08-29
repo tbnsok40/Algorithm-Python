b73a0fd24a2f76741d3dc71869f3dca64fe55af2
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-08-29 11:39:56 +0900

    두개이하로 다른 비트, 비트조작

--- 21AUG/29AUG 두개 이하로 다른 비트.py
+++ 21AUG/29AUG 두개 이하로 다른 비트.py
@@ -1,32 +1,14 @@
-
-def calculate(num_bin, new_bin):
-    count = 0
-    for a, b in zip(num_bin, new_bin):
-        if a != b:
-            count += 1
-    if count <= 2:
-        return True
-    else:
-        False
-
-
 def solution(numbers):
     answer = []
-    for num in numbers:
-        bin_num = bin(num)[2:]
-        while True:
-            num += 1
-            bin_new = bin(num)[2:]
-            diff = len(bin_new) - len(bin_num)
-            if diff > 0:
 
-                if calculate('0' * diff + bin_num, bin_new):
-                    answer.append(num)
-                    break
-            else:
-                if calculate(bin_num, bin_new):
-                    answer.append(num)
-                    break
+    for number in numbers:
+        bin_number = list('0' + bin(number)[2:])
+        idx = ''.join(bin_number).rfind('0')
+        bin_number[idx] = '1'
+
+        if number % 2 == 1:
+            bin_number[idx + 1] = '0'
+        answer.append(int(''.join(bin_number), 2))
 
     return answer
 
@@ -34,3 +16,5 @@ def solution(numbers):
 numbers = [2, 7]
 
 print(solution(numbers))
+# 순회하지 말고 조작하라
+
