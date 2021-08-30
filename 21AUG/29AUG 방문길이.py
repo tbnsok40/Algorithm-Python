fc066d09090f16062b5af8ff5fdc121bf4936898
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-08-30 17:01:33 +0900

    변수 새로 정의

--- 21AUG/29AUG 방문길이.py
+++ 21AUG/29AUG 방문길이.py
@@ -10,13 +10,13 @@ def solution(dirs):
 
     for dir in dirs:
         if (-5 <= result[0] + dir_dict[dir][0] <= 5) and (-5 <= result[1] + dir_dict[dir][1] <= 5):
-            a = result[0] + dir_dict[dir][0]
-            b = result[1] + dir_dict[dir][1]
+            new_x = result[0] + dir_dict[dir][0]
+            new_y = result[1] + dir_dict[dir][1]
 
-            route[str(result[0]) + str(result[1]) + str(a) + str(b)] = 1
-            route[str(a) + str(b) + str(result[0]) + str(result[1])] = 1
+            route[str(result[0]) + str(result[1]) + str(new_x) + str(new_y)] = 1
+            route[str(new_x) + str(new_y) + str(result[0]) + str(result[1])] = 1
 
-            result[0] = a
-            result[1] = b
+            result[0] = new_x
+            result[1] = new_y
 
     return len(route) // 2
