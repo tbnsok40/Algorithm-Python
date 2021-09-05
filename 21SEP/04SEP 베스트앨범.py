31f6fe4f59436e309132fe6402d8db1af8529a41
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-09-05 14:18:51 +0900

    코드 정리

--- 21SEP/04SEP 베스트앨범.py
+++ 21SEP/04SEP 베스트앨범.py
@@ -4,13 +4,15 @@ from collections import defaultdict
 def solution(genres, plays):
     genres_dict = defaultdict(list)
     plays_enum = []
+    # 곡 수 인덱스 추가
     for idx, ele in enumerate(plays):
         plays_enum.append((idx, ele))
+    # 각 장르별 총 곡 수
     sums = dict()
     for k, v in zip(genres, plays):
         try:
             sums[k] += v
-        except:
+        except KeyError:
             sums[k] = v
 
     # 장르 순서대로 배열
@@ -19,6 +21,7 @@ def solution(genres, plays):
         tem.append((k, v))
     tem = sorted(tem, key=lambda x: -x[1])
 
+    # 장르 별 인덱스, 곡 수 정리
     for k, v in zip(genres, plays_enum):
         genres_dict[k].append(v)
 
