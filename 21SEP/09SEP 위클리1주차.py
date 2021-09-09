9ccb7fe868ebe807a2fcdd744d5d7dc205f81a02
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-09-10 00:07:44 +0900

    위클리1주차

---
+++ 21SEP/09SEP 위클리1주차.py
@@ -0,0 +1,10 @@
+def solution(price, money, count):
+    answer = sum([(price * i) for i in range(1, count + 1)]) - money
+    return answer if answer >= 0 else 0
+
+
+price = 3
+money = 20
+count = 4
+
+print(solution(price, money, count))
