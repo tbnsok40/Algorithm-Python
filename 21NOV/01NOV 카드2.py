e84b57ada0cfdad506614b45ec3489a6f05ecaba
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-11-01 06:53:02 +0900

    반례

--- 21NOV/01NOV 카드2.py
+++ 21NOV/01NOV 카드2.py
@@ -4,11 +4,13 @@ from collections import deque
 N = int(sys.stdin.readline())
 
 queue = deque()
-
+if N == 1:
+    print(1)
+    sys.exit()
 for i in range(1, N + 1):
     queue.append(i)
 while len(queue) != 2:
     queue.popleft()
     queue.append(queue.popleft())
+queue.popleft()
 print(queue[-1])
-
