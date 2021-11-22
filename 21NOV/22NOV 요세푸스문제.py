56bab4fbbebfd6c523945445acb5c8a4873a1c73
tbnsok40 <tbnsok40@naver.com>
2021-11-22 23:38:40 +0900

    deque

--- 21NOV/22NOV 요세푸스문제.py
+++ 21NOV/22NOV 요세푸스문제.py
@@ -7,25 +7,19 @@ input = lambda: sys.stdin.readline().rstrip()
 
 def __main__():
     N, K, = map(int, input().split())
-    queue = deque()
     result = []
-    for i in range(1, N + 1):
-        queue.append(i)
+    queue = deque(i for i in range(1, N + 1))
 
-    while len(queue) >= K:
-
+    while queue:
         for _ in range(K - 1):
-            head = queue.popleft()
-            queue.append(head)
-        removed = queue.popleft()
-        result.append(removed)
-    result += list(queue)
+            queue.append(queue.popleft())
+        result.append(queue.popleft())
+
     print("<", end='')
     for i in range(len(result)):
         if i == len(result) - 1:
             print(result[i], end='>')
             exit(0)
-
         print(result[i], end=', ')
 
 
